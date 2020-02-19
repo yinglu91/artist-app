06e689cf8affd6195a1a0162ed94137c04dad191
Ying Lu <yinglu91@gmail.com>
2020-02-18 19:52:54 -0500

    Updates

---
+++ precache-manifest.3d30945ff34e07e60bd1d9d8ff552c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daed4d27a8587fec7ca485c3f0cc57d8",
+    "url": "/artist-app/index.html"
+  },
+  {
+    "revision": "523ee34e2fe0ad64acd8",
+    "url": "/artist-app/static/css/main.98ca19fb.chunk.css"
+  },
+  {
+    "revision": "fed20e117cc15d85998d",
+    "url": "/artist-app/static/js/2.2fd3ba4a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/artist-app/static/js/2.2fd3ba4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "523ee34e2fe0ad64acd8",
+    "url": "/artist-app/static/js/main.46ee75d3.chunk.js"
+  },
+  {
+    "revision": "07bb0da25210f4ac2f5a",
+    "url": "/artist-app/static/js/runtime-main.3cb956d3.js"
+  }
+]);
